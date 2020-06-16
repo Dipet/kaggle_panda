5319b177b26ad03071359903738f336a8c157a1c
Dipet <dipetm@gmail.com>
2020-06-16 16:20:22 +0300

    fix tiles

--- scripts/create_tiles.py
+++ scripts/create_tiles.py
@@ -8,6 +8,7 @@ from skimage.io import MultiImage
 import cv2 as cv
 
 from utils.data_utils import get_tile
+import matplotlib.pyplot as plt
 
 images_dir = "../input/prostate-cancer-grade-assessment/train_images"
 output_dir = "../input/256_36_hsv"
@@ -20,6 +21,11 @@ os.makedirs(output_dir, exist_ok=True)
 
 for path, boxes in tqdm(data.items()):
     img = MultiImage(os.path.join(images_dir, path) + ".tiff")[1]
+    plt.imshow(img)
+    plt.show()
     img = get_tile(img, boxes, 256, 36)
-    img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+    img = 255 - cv.cvtColor(img, cv.COLOR_RGB2BGR)
+    plt.imshow(img)
+    plt.show()
+    break
     cv.imwrite(os.path.join(output_dir, path) + ".png", img)
