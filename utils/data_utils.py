2c2baa6bbfd2ecb52358adedb1eb757876007d68
Dipet <dipetm@gmail.com>
2020-06-16 15:03:32 +0300

    hsv tiles

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import torch
+from torch.nn import functional
 
 
 def tile(img, sz=128, N=16, transform=None, random=False):
@@ -35,3 +37,98 @@ def tile(img, sz=128, N=16, transform=None, random=False):
         result[y * sz:(y + 1) * sz, x * sz:(x + 1) * sz] = img_i
 
     return result
+
+
+def __rgb_to_hsv(image):
+    if len(image.shape) < 3 or image.shape[-3] != 3:
+        raise ValueError("Input size must have a shape of (3, H, W). Got {}".format(image.shape))
+
+    r, g, b = image
+
+    maxc = image.max(-3)[0]
+    minc = image.min(-3)[0]
+
+    v = maxc  # brightness
+
+    deltac: torch.Tensor = maxc - minc
+
+    s = deltac / v
+    s = torch.where(torch.isnan(s), torch.zeros_like(s), s)
+
+    # avoid division by zero
+    deltac = torch.where(deltac == 0, torch.ones_like(deltac), deltac)
+
+    maxg = g == maxc
+    maxr = r == maxc
+
+    r, g, b = (torch.stack([maxc] * 3) - image) * (1 / deltac)
+
+    h = 4.0 + g - r
+    h = torch.where(maxg, 2.0 + r - b, h)
+    h = torch.where(maxr, b - g, h)
+    h = torch.where(minc == maxc, torch.zeros_like(h), h)
+
+    h *= 60.0
+    h %= 360.0
+
+    return torch.stack([h, s, v], dim=-3)
+
+
+def get_tile(img, boxes, sz=256, num=36):
+    shape = img.shape
+    pad0, pad1 = (sz - shape[0] % sz) % sz, (sz - shape[1] % sz) % sz
+
+    img = np.pad(img, [[pad0 // 2, pad0 - pad0 // 2], [pad1 // 2, pad1 - pad1 // 2], [0, 0]], constant_values=255)
+
+    s = int(np.sqrt(num))
+    tile = np.full([sz * s, sz * s, 3], 255, dtype=img.dtype)
+    for i, box in enumerate(boxes):
+        x = i % s
+        y = i // s
+
+        tile[y * sz:(y + 1) * sz, x * sz:(x + 1) * sz] = img[box[1]:box[3], box[0]:box[2]]
+    return tile
+
+
+def tile_hsv_boxes(img, sz=256, N=36, default_val=255):
+    shape = img.shape
+    pad0, pad1 = (sz - shape[0] % sz) % sz, (sz - shape[1] % sz) % sz
+
+    img = torch.from_numpy(img).cuda().float()
+    img = 255 - __rgb_to_hsv(img.permute([2, 0, 1]) / 255.0)[1] * 255.0
+    img = torch.unsqueeze(img, -1)
+
+    img = functional.pad(img, [0, 0, pad1 // 2, pad1 - pad1 // 2, pad0 // 2, pad0 - pad0 // 2], value=default_val)
+
+    shape = img.shape
+
+    img = img.reshape(img.shape[0] // sz, sz, img.shape[1] // sz, sz, 1)
+    img = img.permute([0, 2, 1, 3, 4]).reshape(-1, sz, sz, 1)
+
+    num = len(img)
+
+    if len(img) < N:
+        img = functional.pad(img, [0, 0, 0, 0, 0, 0, 0, N - len(img)], value=default_val)
+
+    idxs = torch.argsort(img.reshape(img.shape[0], -1).float().sum(-1))[:N]
+    del img
+
+    res_boxes = []
+    idxs = idxs.detach().cpu().numpy()
+
+    indexes = np.arange(N)
+
+    for i, j in enumerate(indexes):
+        if i >= num:
+            break
+
+        x = idxs[i] % (shape[1] // sz)
+        y = idxs[i] // (shape[1] // sz)
+        x1 = x * sz
+        x2 = (x + 1) * sz
+        y1 = y * sz
+        y2 = (y + 1) * sz
+
+        res_boxes.append([x1, y1, x2, y2])
+
+    return res_boxes
